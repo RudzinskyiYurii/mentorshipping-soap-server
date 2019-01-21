6563ad893745860a8871010e32befd66d9e6d6cc
Yurii_Rudzinskii <Yurii_Rudzinskii@epam.com>
2019-01-21 23:53:24 +0200

    Small refactoring

--- src/main/java/com/example/biographysoap/service/AuthorService.java
+++ src/main/java/com/example/biographysoap/service/AuthorService.java
@@ -17,30 +17,6 @@ public class AuthorService {
     @Autowired
     private AuthorModelService authorModelService;
 
-    @PostConstruct
-    public void init(){
-
-        Author authorTest1 = new Author();
-        authorTest1.setAuthorId(1000L);
-        authorTest1.setName("Jessalyn Divis");
-        authorTest1.setBio("This is test author 1 bio");
-
-        Author authorTest2 = new Author();
-        authorTest2.setAuthorId(1002L);
-        authorTest2.setName("Rianon Yelland");
-        authorTest2.setBio("This is test author 2 bio");
-
-        Author authorTest3 = new Author();
-        authorTest3.setAuthorId(1004L);
-        authorTest3.setName("Cris Cabrara");
-        authorTest3.setBio("This is test author 3 bio");
-
-        authors.put(authorTest1.getName(), authorTest1);
-        authors.put(authorTest2.getName(), authorTest2);
-        authors.put(authorTest3.getName(), authorTest3);
-    }
-
-
     public Author getAuthor(String name) {
         AuthorModel authorModel = authorModelService.findByName(name);
         return convertAuthorModelToAuthor(authorModel);
